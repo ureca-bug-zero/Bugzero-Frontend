d7de5093cf6e7344ce0b5dd13e035aa1513aea3e
Jetty-Lee <jetty0188@gmail.com>
2025-05-07 15:34:58 +0900

    feat: tailwind primary color, secondary color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: '#1AE273',
+          200: '#A3F5C7',
+        },
+        secondary: {
+          500: '#606060',
+          200: '#E7BEEE',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
