5d83963da4bd6f2fbbd2c1cc5021dc8d6c8ca3c0
SujinRo <shtnwls1111@ewhain.net>
2025-05-11 04:57:36 +0900

    feat: #20 globals.css(colors 설정)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -18,6 +19,17 @@ export default {
       'pretendard-semibold': ['Pretendard-SemiBold'],
       'pretendard-bold': ['Pretendard-Bold'],
       'pretendard-extrabold': ['Pretendard-ExtraBold'],
+    },
+    colors: {
+      primary: 'rgb(26, 226, 115)' /*bg*/,
+      secondary: 'rgb(51, 51, 51)' /*text*/,
+      error: 'rgb(255, 204, 204)',
+      success: 'rgb(26, 226, 115)',
+      'gray-700': 'rgb(96, 96, 96)',
+      'gray-200': 'rgb(231, 235, 238)',
+      'gray-100': 'rgb(233, 233, 233)',
+      white: 'rgb(255, 255, 255)',
+      transparent: 'transparent',
     },
   },
   plugins: [],
