369b3e3da6097e4e397857e7de0a893a9f766f98
SujinRo <shtnwls1111@ewhain.net>
2025-05-11 03:35:41 +0900

    feat: #20 globals.css(font 및 전체 css 설정)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    screen: {
+      phone: '393px',
+      tablet: '768px',
+      desktop: '1440px',
+    },
+    fontFamily: {
+      'inter-regular': ['Inter-Regular'],
+      'inter-medium': ['Inter-Medium'],
+      'inter-semibold': ['Inter-SemiBold'],
+      'inter-bold': ['Inter-Bold'],
+      'pretendard-regular': ['Pretendard-Regular'],
+      'pretendard-medium': ['Pretendard-Medium'],
+      'pretendard-semibold': ['Pretendard-SemiBold'],
+      'pretendard-bold': ['Pretendard-Bold'],
+      'pretendard-extrabold': ['Pretendard-ExtraBold'],
+    },
   },
   plugins: [],
-}
-
+};
