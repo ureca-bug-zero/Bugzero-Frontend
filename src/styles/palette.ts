34d26d561cda734a35fb3d012adf26f763141790
SujinRo <shtnwls1111@ewhain.net>
2025-05-11 17:25:12 +0900

    feat: #20 globals.css에서 설정하던 typo와 tailwind.config.js에서만 설정하던 palette 함수로 설정

---
+++ src/styles/palette.ts
@@ -0,0 +1,11 @@
+export const palette = {
+  Primary: 'text-primary',
+  Secondary: 'text-secondary',
+  Error: 'text-error',
+  Success: 'text-success',
+  Gray1: 'text-gray-700',
+  Gray2: 'text-gray-200',
+  Gray3: 'text-gray-100',
+  White: 'text-white',
+  Transparent: 'text-transparent',
+};
---
+++ src/styles/typo.ts
@@ -0,0 +1,73 @@
+export const typo = {
+  /*
+  Heading1_Eng/Kor: title
+  Heading2_Eng/Kor: greeting
+  Heading3_Eng: subtitle
+  Heading3_Kor: ranking
+  Heading4_Eng: modal-title
+  Heading4_Kor: ranking-des
+  Heading5: calendar-title
+  Heading6: timer
+  Heading7: landing-desc
+  Nav_Eng: header
+  Body1: mission-todo
+  Body2: todo
+  Label1_Eng: confirm-btn
+  Label1_Kor: calendar-date
+  Label2_Eng/Kor: modal-desc
+  Label3_Eng: email
+  Label3_Kor: name / no-friend-msg
+  Label4_Eng: add-btn
+  Label4_Eng: input
+  Label5_Eng: modal-email
+  Label5_Kor: modal-name
+  Label6: modal-input
+  Label7: toast-msg
+  Label8: modal-btn
+  */
+
+  Heading1_Eng:
+    'font-inter-bold text-[5rem] leading-[1.5rem] tablet:text-[8rem]',
+  Heading1_Kor:
+    'font-pretendard-extrabold text-[2rem] leading-[2.7rem] tablet:text-[3rem] tablet:leading-[4.05rem] desktop:text-[5rem] desktop:leading-[6.75rem]',
+  Heading2_Eng:
+    'font-inter-bold text-[1.5rem] leading-[1.5rem] tablet:text-[2.25rem]',
+  Heading2_Kor:
+    'font-pretendard-bold text-[1.5rem] leading-[1.5rem] tablet:text-[2.25rem]',
+  Heading3_Eng:
+    'font-inter-semibold text-[1.5rem] leading-[1.5rem] tablet:text-[2rem]',
+  Heading3_Kor:
+    'font-pretendard-bold text-[0.9375rem] leading-[1.1875rem] tablet:text-[1.5rem]',
+  Heading4_Eng:
+    'font-inter-medium text-[0.98188rem] leading-[0.775rem] tablet:text-[1.5rem] tablet:leading-[1.1875rem]',
+  Heading4_Kor:
+    'font-pretendard-regular text-[0.875rem] leading-[1.1875rem] tablet:text-[1.25rem]',
+  Heading5: 'font-pretendard-bold text-[1.1875rem]',
+  Heading6: 'font-pretendard-bold text-[1.125rem] leading-[3rem]',
+  Heading7:
+    'font-pretendard-bold text-[0.875rem] leading-[2.8125rem] tablet:text-[1.125rem]',
+  Nav: 'font-inter-semibold text-[0.9375rem]',
+  Body1: 'font-pretendard-bold text-[0.75rem]',
+  Body2: 'font-pretendard-medium text-[0.75rem] leading-[1.5rem]',
+  Label1_Eng:
+    'font-inter-semibold text-[0.65438rem] leading-[0.98125rem] tablet:text-[1rem] tablet:leading-[1.5rem]',
+  Label1_Kor: 'font-pretendard-semibold text-[0.78rem]',
+  Label2_Eng:
+    'font-inter-medium text-[0.5725rem] leading-[0.775rem] tablet:text-[0.875rem] tablet:leading-[1.1875rem]',
+  Label2_Kor:
+    'font-pretendard-medium text-[0.5725rem] leading-[0.775rem] tablet:text-[0.875rem] tablet:leading-[1.1875rem]',
+  Label2_Bold_Kor:
+    'font-pretendard-bold text-[0.5725rem] leading-[0.775rem] tablet:text-[0.875rem] tablet:leading-[1.1875rem]',
+  Label3_Eng: 'font-inter-regular text-[0.8125rem] leading-[3rem]',
+  Label3_Kor: 'font-pretendard-regular text-[1.0625rem] leading-[3rem]',
+  Label4_Eng: 'font-inter-medium text-[0.75rem] leading-[1.5rem]',
+  Label4_Kor: 'font-pretendard-medium text-[0.75rem] leading-[1.5rem]',
+  Label5_Eng:
+    'font-inter-regular text-[0.49062rem] leading-[0.65625rem] tablet:text-[0.75rem] tablet:leading-[1rem]',
+  Label5_Kor:
+    'font-pretendard-medium text-[0.65438rem] leading-[0.98125rem] tablet:text-[1rem] tablet:leading-[1.5rem]',
+  Label6: 'font-pretendard-regular text-[0.65438rem] tablet:text-[1rem]',
+  Label7: 'font-pretendard-regular text-[0.60688rem] tablet:text-[1rem]',
+  Label8:
+    'font-pretendard-regular text-[0.40937rem] leading-[0.775rem] tablet:text-[0.625rem] tablet:leading-[1.1875rem]',
+};
