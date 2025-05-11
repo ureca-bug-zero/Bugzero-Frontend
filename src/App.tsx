7445c3f77b7aabfc73ddc8c32f40bfd682986ac1
SujinRo <shtnwls1111@ewhain.net>
2025-05-11 14:03:54 +0900

    fix: #20 globals.css(font_media-query 에러 수정)

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import './styles/tailwind.css';
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="Heading1_Eng">Vite + React</div>
+      <div className="Heading1_Kor">Vite + React</div>
+      <div className="Heading2_Eng">Vite + React</div>
+      <div className="Heading2_Kor">Vite + React</div>
+      <div className="Heading3_Eng">Vite + React</div>
+      <div className="Heading3_Kor">Vite + React</div>
+      <div className="Heading4_Eng">Vite + React</div>
+      <div className="Heading4_Kor">Vite + React</div>
+      <div className="Heading5">Vite + React</div>
+      <div className="Heading6">Vite + React</div>
+      <div className="Heading7">Vite + React</div>
+      <div className="Nav">Vite + React</div>
+      <div className="Label1_Eng">Vite + React</div>
+      <div className="Label1_Kor">Vite + React</div>
+      <div className="Label2_Eng">Vite + React</div>
+      <div className="Label2_Kor">Vite + React</div>
+      <div className="Label2_Bold_Kor">Vite + React</div>
+      <div className="Label3_Eng">Vite + React</div>
+      <div className="Label3_Kor">Vite + React</div>
+      <div className="Label4_Eng">Vite + React</div>
+      <div className="Label4_Kor">Vite + React</div>
+      <div className="Label5_Eng">Vite + React</div>
+      <div className="Label5_Kor">Vite + React</div>
+      <div className="Label6">Vite + React</div>
+      <div className="Label7">Vite + React</div>
+      <div className="Label8">Vite + React</div>
     </>
   );
 }
