a7bc3b780b8d47b805eb4293301704c87f953372
EUNCHAE LEE <leesilver7@gmail.com>
2025-05-12 13:27:06 +0900

    feat: #24 홈페이지 기본 구조 구현

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -8,14 +8,15 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="main-layout">
+    <div className="flex flex-col h-screen">
       <header>
         {/* <UserGreeting /> */}
         {/* 헤더 넣기! */}
         <Header />
       </header>
-      <main>{children}</main> {/* children 부분 렌더링 */}
-      <footer>
+      <main className="flex-1 overflow-hidden">{children}</main>{' '}
+      {/* children 부분 렌더링 */}
+      <footer className="h-12 bg-gray-100 text-center text-xs flex items-center justify-center text-gray-500">
         <p>© 2025 My App</p>
       </footer>
     </div>
