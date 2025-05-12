d83ddb4751ef65a93ed1f5400516885091589ad0
ahn_minji <ahn_minji@naver.com>
2025-05-12 17:50:30 +0900

    fix: #30 페이지 랜더링 최소화

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,65 +3,61 @@ import MainLayout from '@/components/layout/MainLayout';
 import UserPanel from '@/components/panels/UserPanel';
 import TodoPanel from '@/components/panels/TodoPanel';
 import RightPanel from '@/components/panels/RightPanel';
+import { useViewport } from '@/hooks/useViewport'; // 뷰포트 감지 훅
 
 const HomePage: React.FC = () => {
+  const viewport = useViewport(); // 'mobile' | 'tablet' | 'desktop'
+
+  const renderPanels = () => {
+    if (viewport === 'desktop') {
+      return (
+        <div className="flex justify-center w-full">
+          <div className="flex">
+            <div className="pr-[35px] pl-[35px] w-[470px]">
+              <UserPanel />
+            </div>
+            <div className="pr-[35px] pl-[35px] w-[470px]">
+              <TodoPanel />
+            </div>
+            <div className="pr-[35px] pl-[35px] w-[470px]">
+              <RightPanel />
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    if (viewport === 'tablet') {
+      return (
+        <div className="flex justify-center px-6 gap-8">
+          <div className="px-[35px] w-[360px]">
+            <UserPanel />
+          </div>
+          <div className="px-[35px] w-[360px]">
+            <TodoPanel />
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="flex flex-col items-center gap-6">
+        <UserPanel />
+        <TodoPanel />
+        <RightPanel />
+      </div>
+    );
+  };
+
   return (
     <MainLayout>
-      <div className="pt-[112px] px-4 lg:px-0">
-        {/* ✅ 데스크탑 레이아웃 */}
-        <div className="hidden lg:flex justify-center w-full">
-          <div className="flex">
-            {/* 왼쪽: 유저 패널 */}
-            <div className="pr-[70px]">
-              <div className="w-[360px]">
-                <UserPanel />
-              </div>
-            </div>
-
-            {/* 가운데: 투두 패널 */}
-            <div className="px-[70px]">
-              <div className="w-[360px]">
-                <TodoPanel />
-              </div>
-            </div>
-
-            {/* 오른쪽: 친구 패널 */}
-            <div className="pl-[70px]">
-              <div className="w-[360px]">
-                <RightPanel />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* ✅ 태블릿 이하 */}
-        <div className="flex flex-col lg:hidden gap-8">
-          {/* 태블릿: 유저 + 투두 나란히 */}
-          <div className="hidden md:flex justify-center">
-            <div className="pr-[70px]">
-              <div className="w-[360px]">
-                <UserPanel />
-              </div>
-            </div>
-            <div className="pl-[70px]">
-              <div className="w-[360px]">
-                <TodoPanel />
-              </div>
-            </div>
-          </div>
-
-          {/* 모바일: 세로 정렬 */}
-          <div className="md:hidden flex flex-col items-center gap-6">
-            <UserPanel />
-            <TodoPanel />
+      <div className="pt-[112px] px-4 lg:px-0 flex flex-col gap-8">
+        {renderPanels()}
+        {viewport === 'tablet' && (
+          <div className="w-full px-6">
             <RightPanel />
           </div>
-
-          {/* 태블릿 전용 친구 패널 하단 배치 */}
-          <div className="hidden md:block w-full px-6">
-            <RightPanel />
-          </div>
-        </div>
+        )}
       </div>
     </MainLayout>
   );
