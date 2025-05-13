0d02cb0964e773d523c5f5a86cc0f02cdbcfd37a
SujinRo <shtnwls1111@ewhain.net>
2025-05-13 16:07:47 +0900

    feat: #36 Calendar 헤더 부분 style 변경 및 색상 추가

--- src/components/home/CalendarBox.tsx
+++ src/components/home/CalendarBox.tsx
@@ -2,21 +2,57 @@ import Calendar from 'react-calendar';
 import { format } from 'date-fns';
 import rightIcon from '@/assets/icons/home/right-icon.svg';
 import leftIcon from '@/assets/icons/home/left-icon.svg';
+import clsx from 'clsx';
+import { theme } from '../../styles/theme';
+import { Flex, Position } from '../common/Wrapper';
 
 export default function CalendarBox() {
+  const circle = clsx(
+    theme.bgPalette.Primary,
+    'w-[15px] h-[15px] rounded-full',
+  );
+
+  const CircleList = () => {
+    return (
+      <div
+        className={clsx(
+          Flex({ direction: 'rowReverse', gap: 'gap-[3px]' }),
+          'w-[93px] h-[37px]',
+        )}
+      >
+        {/*_ : 요소 안 쓰니까 무시*/}
+        {Array.from({ length: 5 }).map((_, i) => (
+          <div key={i} className={circle} style={{ opacity: (i + 1) * 0.2 }} />
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <Calendar
-      locale="ko"
-      defaultView="month"
-      formatShortWeekday={(locale, date) =>
-        ['토', '일', '월', '화', '수', '목', '금'][date.getDay()]
-      }
-      formatDay={(locale, date) => format(date, 'd')}
-      formatMonthYear={(locale, date) => format(date, 'yyyy년 M월')}
-      nextLabel={<img src={rightIcon} alt="right" />}
-      prevLabel={<img src={leftIcon} alt="left" />}
-      next2Label={null}
-      prev2Label={null}
-    />
+    <>
+      <div
+        className={clsx(Position({ position: 'relative' }), 'w-[276.19781px]')}
+      >
+        <Calendar
+          locale="ko"
+          defaultView="month"
+          calendarType="gregory"
+          formatShortWeekday={(locale, date) =>
+            ['일', '월', '화', '수', '목', '금', '토'][date.getDay()]
+          }
+          formatDay={(locale, date) => format(date, 'd')}
+          formatMonthYear={(locale, date) => format(date, 'yyyy년 M월')}
+          nextLabel={<img src={rightIcon} alt="right" />}
+          prevLabel={<img src={leftIcon} alt="left" />}
+          next2Label={null}
+          prev2Label={null}
+        />
+        <div
+          className={clsx(Position({ position: 'absolute' }), 'right-0 top-0')}
+        >
+          <CircleList />
+        </div>
+      </div>
+    </>
   );
 }
