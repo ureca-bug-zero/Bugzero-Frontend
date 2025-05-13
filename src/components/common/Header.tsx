ec964550f348993a83364699f1c7f78a4c18d635
Jetty-Lee <jetty0188@gmail.com>
2025-05-13 09:49:41 +0900

    feat: #2 header 로그아웃 버튼 조건부 렌더링 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,24 @@
-// src/components/common/Header.tsx
 import React from 'react';
+import { useAuthStore } from '@/store/auth';
+// import { useNavigate } from 'react-router-dom';
 import logo from '@/assets/logo.png';
 
 const Header = () => {
+  const { isLoggedIn } = useAuthStore();
+  // const navigate = useNavigate();
   return (
-    <header className="w-full bg-secondary-600 text-white px-8 py-4 flex justify-between">
+    <header className="w-full bg-secondary-600 text-white px-8 py-4 flex justify-between select-none">
       {/* 로고 이미지 */}
       <div className="flex items-center gap-2">
         <img src={logo} alt="BugZero Logo" className="h-8" />
       </div>
 
       {/* 로그아웃 버튼 */}
-      <button className="bg-secondary-600 hover: text-white px-4 py-2 rounded transition font-medium">
-        Logout
-      </button>
+      {isLoggedIn && (
+        <button className="bg-secondary-600 hover: text-white rounded transition font-pretendard text-[15px]">
+          Logout
+        </button>
+      )}
     </header>
   );
 };
