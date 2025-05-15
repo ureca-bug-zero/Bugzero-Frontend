ed38ef03ad76a40e3a89ada10a5c7286eb731d97
sujinRo <shtnwls1111@ewhain.net>
2025-05-15 14:57:00 +0900

    style: #78 Friend Item Component에 prefix 추가

--- src/components/home/FriendItem.tsx
+++ src/components/home/FriendItem.tsx
@@ -9,8 +9,8 @@ interface FriendItemProps {
 
 export default function FriendItem({ name, email }: FriendItemProps) {
   return (
-    <div className={clsx(Flex({ direction: 'column', align: 'start', width: 'w-[250px]', height: 'h-[80px]', padding: { x: 'px-[20px]' } }), 'hover:bg-gray-100 rounded-[5px]')}>
-      <p className={clsx(Flex({justify: 'start', width: 'w-full', height: 'h-[30px]'}), theme.typo.Label3_Kor,)}>{name}</p>
-      <p className={clsx(Flex({justify: 'start', width: 'w-full', height: 'h-[30px]'}), theme.typo.Label3_Eng, theme.textPalette.Gray1, '-translate-y-[4px] h-[30px]')}>{email}</p>
+    <div className={clsx(Flex({wrap: 'wrap', width: 'w-[242px] tablet:w-[250px] ', height: 'h-[54px] tablet:h-[80px]', padding: { x: 'tablet:px-[20px]' } }), 'tablet:hover:bg-gray-100 rounded-[5px]')}>
+      <p className={clsx(Flex({wrap: 'wrap', justify: 'start', align: 'center', width: 'w-[56px] tablet:w-[290px]', height: 'h-[30px]'}), theme.typo.Label3_Kor)}>{name}</p>
+      <p className={clsx(Flex({wrap: 'wrap', justify: 'start', align: 'center', width: 'w-[135px] tablet:w-[290px]', height: 'h-[30px]'}), theme.typo.Label3_Eng, theme.textPalette.Gray1, 'ml-[51px] tablet:-translate-y-[4px] h-[30px] ml-0')}>{email}</p>
     </div>);
 }
