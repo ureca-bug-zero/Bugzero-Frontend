80f3a3ff09575941a3cbd19fc587aa35cc66a20f
sujinRo <shtnwls1111@ewhain.net>
2025-05-15 16:38:47 +0900

    fix: #78 배포 에러 해결 - 사용하지 않는 값 제거

--- src/components/home/FriendBox.tsx
+++ src/components/home/FriendBox.tsx
@@ -1,11 +1,18 @@
 import clsx from "clsx";
 import { Flex } from "../common/Wrapper";
-import FriendItem, { FriendItemProps } from "./FriendItem";
+import FriendItem from "./FriendItem";
 import { theme } from "../../styles/theme";
-import { useState } from "react";
+// import { useEffect, useState } from "react";
 
 export default function FriendBox() {
-  const [friendList, setFriendList] = useState<FriendItemProps[]>([]);
+  // const [friendList, setFriendList] = useState<FriendItemProps[]>([]);
+
+  const friendList = [
+    { name: '노수진', email: 'shtnwls1111@naver.com' },
+    { name: '노수진', email: 'shtnwls1111@naver.com' },
+    { name: '노수진', email: 'shtnwls1111@naver.com' },
+    { name: '노수진', email: 'shtnwls1111@naver.com' },
+  ]
 
   return (
     <div className={clsx(Flex({ direction: 'column', justify: 'start', width: 'w-[276px] tablet:w-[292px]', height: 'h-[162px] tablet:h-[240px]', padding: {x: 'px-[17px] tablet:px-[0px]'} }), 'friendList.length > 0 ? bg-gray-100 overflow-y-scroll scrollbar-hide rounded-[5px] : bg-transparent tablet:bg-transparent')}>
