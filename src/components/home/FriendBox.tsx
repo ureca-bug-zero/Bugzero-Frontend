ade199b3aa1a9ec2c2891f490c3f32631a7f41fb
sujinRo <shtnwls1111@ewhain.net>
2025-05-15 16:04:51 +0900

    style: #78 prefix style 추가 및 글씨에 break-all 추가

--- src/components/home/FriendBox.tsx
+++ src/components/home/FriendBox.tsx
@@ -12,7 +12,7 @@ const friendList = [
 export default function FriendBox() {
 
   return (
-    <div className={clsx(Flex({ direction: 'column', justify:'start', width: 'w-[276px] tablet:w-[292px]', height: 'h-[162px] tablet:h-[240px]' }), 'bg-gray-100 overflow-y-scroll scrollbar-hide rounded-[5px] tablet:bg-transparent')}>
+    <div className={clsx(Flex({ direction: 'column', justify: 'start', width: 'w-[276px] tablet:w-[292px]', height: 'h-[162px] tablet:h-[240px]', padding: {x: 'px-[17px] tablet:px-[0px]'} }), 'bg-gray-100 overflow-y-scroll scrollbar-hide rounded-[5px] tablet:bg-transparent')}>
       {friendList.map((item, idx) => (
         <FriendItem key={idx} name={item.name} email={item.email}/>)
       )}
--- src/components/home/FriendItem.tsx
+++ src/components/home/FriendItem.tsx
@@ -9,8 +9,8 @@ interface FriendItemProps {
 
 export default function FriendItem({ name, email }: FriendItemProps) {
   return (
-    <div className={clsx(Flex({wrap: 'wrap', width: 'w-[242px] tablet:w-[250px] ', height: 'min-h-[54px] tablet:min-h-[80px]', padding: { x: 'tablet:px-[20px]' } }), 'tablet:hover:bg-gray-100 rounded-[5px]')}>
-      <p className={clsx(Flex({wrap: 'wrap', justify: 'start', align: 'center', width: 'w-[56px] tablet:w-[290px]', height: 'h-[30px]'}), theme.typo.Label3_Kor)}>{name}</p>
-      <p className={clsx(Flex({wrap: 'wrap', justify: 'start', align: 'center', width: 'w-[135px] tablet:w-[290px]', height: 'h-[30px]'}), theme.typo.Label3_Eng, theme.textPalette.Gray1, 'ml-[51px] tablet:-translate-y-[4px] h-[30px] ml-[0px]')}>{email}</p>
+    <div className={clsx(Flex({wrap: 'wrap', width: 'w-[242px] tablet:w-[250px] ', height: 'min-h-[54px] tablet:min-h-[80px]', padding: { x: 'tablet:px-[20px]' } }), 'tablet:hover:bg-gray-100 tablet:rounded-[5px]')}>
+      <p className={clsx(Flex({justify: 'start', align: 'center', width: 'w-[56px] tablet:w-[290px]', height: 'h-[30px]'}), theme.typo.Label3_Kor, 'break-all leading-0')}>{name}</p>
+      <p className={clsx(Flex({justify: 'start', align: 'center', width: 'w-[135px] tablet:w-[290px]', height: 'h-[30px]'}), theme.typo.Label3_Eng, theme.textPalette.Gray1, 'ml-[51px] tablet:-translate-y-[4px] break-all leading-0 tablet:h-[30px] tablet:ml-[0px]')}>{email}</p>
     </div>);
 }
