d5fd4e7965e1e4687c3d310c83f3ed58f5c2df07
sujinRo <shtnwls1111@ewhain.net>
2025-05-16 17:40:03 +0900

    feat: #94 로그아웃 연결

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,8 +4,11 @@ import { theme } from '../../styles/theme';
 import { CommonProps } from './types';
 import logo from '@/assets/icons/logo.png';
 import { Link } from 'react-router-dom';
+import { useUserStore } from '../../store/user';
 
 export default function Header({ type }: CommonProps) {
+  const setToken = useUserStore((state) => state.setToken);
+
   return (
     <header
       className={clsx(
@@ -31,7 +34,13 @@ export default function Header({ type }: CommonProps) {
         <img src={logo} alt="BugZero" className="w-[158px] h-[44px]" />
       </Link>
       {type === 'home' && (
-        <button className={clsx(theme.textPalette.White, theme.typo.Nav)}>
+        <button
+          className={clsx(theme.textPalette.White, theme.typo.Nav)}
+          onClick={() => {
+            window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/kakao/logout`;
+            setToken('');
+          }}
+        >
           Logout
         </button>
       )}
