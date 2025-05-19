ae3e2a5ac67ff8c17fb400f8d04ab6bdd8b02058
sujinRo <shtnwls1111@ewhain.net>
2025-05-19 11:34:32 +0900

    fix: #104 배포 에러 해결

--- src/pages/friend-home.tsx
+++ src/pages/friend-home.tsx
@@ -1,53 +1,14 @@
 import clsx from 'clsx';
 import GreetingBox from '../components/home/GreetingBox';
 import { Flex, Position } from '../components/common/Wrapper';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { FriendTodoTemplate } from '../components/friend/FriendTodoTemplate';
 import FriendCalendarBox from '../components/friend/FriendCalendarBox';
-import { CalendarData } from '../types/home';
-import { useDateStore } from '../store/dateStore';
-import { friendCalendar } from '../apis/friend';
-import { useMutation } from '@tanstack/react-query';
-import { format } from 'date-fns';
-import { useUserStore } from '../store/userStore';
 
 export default function FriendPage() {
   const params = useParams();
-  const token = useUserStore((state) => state.token);
   const [isClicked, setIsClicked] = useState<boolean>(false);
-  const [calendarList, setCaldendarList] = useState<CalendarData>({});
-  const [activeStartDate, setActiveStartDate] = useState<Date>(new Date());
-  const setFriendSelectedDate = useDateStore(
-    (state) => state.setFriendSelectedDate,
-  );
-
-  const friendCalendarMutation = useMutation({
-    mutationFn: friendCalendar,
-    onSuccess: (data) => {
-      const yearMonth = format(activeStartDate, 'yyyy-MM');
-      const newData: CalendarData = {};
-
-      Object.entries(data.data.score).forEach(([day, percent]) => {
-        const paddedDay = day.padStart(2, '0');
-        const fullDate = `${yearMonth}-${paddedDay}`;
-        newData[fullDate] = percent as number;
-      });
-      setCaldendarList((prev) => ({ ...prev, ...newData }));
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
-
-  useEffect(() => {
-    const yearMonth = format(activeStartDate, 'yyyy-MM');
-    friendCalendarMutation.mutate({
-      friendId: params.friendId,
-      yearMonth: yearMonth,
-      token: token,
-    });
-  }, [activeStartDate]);
 
   const handleOpen = () => {
     setIsClicked(true);
