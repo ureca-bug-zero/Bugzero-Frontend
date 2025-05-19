cb1dd6cb3df1ad87e7d76cfbf597452404a54d23
seohyunn2 <tjgus9138@naver.com>
2025-05-19 14:57:56 +0900

    style: #108 toast message UI 수정

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <ToastContainer
           position="top-center"
           autoClose={3000}
-          hideProgressBar={false}
+          hideProgressBar={true}
           newestOnTop
           closeOnClick
           pauseOnFocusLoss
